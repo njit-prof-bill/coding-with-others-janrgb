4c716ad019b0adf52cfc772e16d70ef5ba786c90
Janish Suneja <js2629@njit.edu>
2024-09-23 18:55:09 -0400

    Put some lines in solution.py

--- solution.py
+++ solution.py
@@ -1,3 +1,17 @@
 def longest_substring_k_unique(s: str, k: int) -> int:
-    # Write your code here
-    pass
+    '''
+    If string is empty, return 0
+    Integer default should be 0 otherwise
+    Min Window size is K, string length must be min K long to count...
+    but start with window size = to length of string. That way we can return immediately if conditions met (only K unique chars)
+    If we get to min window size and there are not K unique chars...return 0.
+    '''
+
+    # String empty? Return 0.
+    if str == "":
+        return 0
+    
+    longest = 0
+    window_size = len(str)
+
+    return 0
